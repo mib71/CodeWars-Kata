814adeb225378f5b1e150a579f8cd46b9c7fe4d1
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 21:30:15 +0100

    Add ShortestWord

---
+++ CSharp/7-kyu/ShortestWord/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShortestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindShort("bitcoin take over the world maybe who knows perhaps"));
+        }
+        public static int FindShort(string s)
+        {
+            int rv = 50;
+            string[] words = s.Split(' ');
+            foreach (string word in words)
+                if (word.Length < rv) rv = word.Length;
+            
+            return rv;
+        }
+    }
+}
