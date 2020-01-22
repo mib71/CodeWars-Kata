521c3837ea607579394a6bffec1a01685ddf084a
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 20:44:36 +0100

    Add SumOfNumbers

---
+++ CSharp/7-kyu/SumOfNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ShowSequence(6));
+        }
+        public static string ShowSequence(int n)
+        {
+            string rv = "";
+            List<string> rvList = new List<string>();
+            int sum = 0;
+
+            if (n < 0) return rv = n + "<0";
+            if (n == 0) return rv = n + "=0";
+
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+                if(i < n)
+                {
+                    rvList.Add(i + "+");
+                }
+                if(i == n)
+                {
+                    rvList.Add(i + " = " + sum);
+                    rv = string.Join("", rvList);
+                }
+                 
+            }
+            
+            return rv;
+        }
+    }
+}
---
+++ CSharp/7-kyu/SumOfNumbers/README.md
@@ -0,0 +1,23 @@
+## 7 kyu Sum of numbers from 0 to N
+
+### Instructions:
+
+We want to generate a function that computes the series starting   
+from 0 and ending until the given number.
+
+### Example:
+```
+Input:> 6 	Output:  0+1+2+3+4+5+6 = 21 
+Input:> -15	Output: -15<0
+Input:> 0	Output: 0=0
+```
+
+```cs
+public class Kata
+{
+  public static string OddOrEven(int[] array)
+  {
+    //enter code here
+  }
+}
+```
