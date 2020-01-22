f7a8f0766b76cb41200162f562cb749b71bfea00
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 19:42:27 +0100

    BinnaryToText

---
+++ CSharp/6-kyu/BinaryToText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinaryToString("0100100001100101011011000110110001101111"));
+        }
+        public static string BinaryToString(string binary)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(binary.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+    }
+}
