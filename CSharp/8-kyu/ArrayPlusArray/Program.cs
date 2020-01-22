83a087a9cf440881894c4b103c80517f15cc188e
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 10:49:33 +0100

    Add 8 kyu Array plus array

---
+++ CSharp/8-kyu/ArrayPlusArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayPlusArray
+{
+    class Program
+    {
+        /** 8 kyu Array plus array
+             * Description:
+             * I'm new to coding and now I want to get the sum of two arrays...actually 
+             * the sum of all their elements. I'll appreciate for your help.
+             * 
+             * P.S. Each array includes only integer numbers. Output is a number too.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArrayPlusArray(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+        }
+        public static int ArrayPlusArray(int[] arr1, int[] arr2)
+        {
+            return arr1.Sum() + arr2.Sum();
+        }
+    }
+}
