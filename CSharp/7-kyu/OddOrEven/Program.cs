e9f9b3f9dcf208408932049b5b3472df063cd39d
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 14:35:07 +0100

    Add 7 kyu Odd or Even

---
+++ CSharp/7-kyu/OddOrEven/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace OddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OddOrEven(new int[]{ 1023, 1, 2}));
+        }
+        public static string OddOrEven(int[] array)
+        {
+            return (array.Sum() % 2 == 0) ? "even" : "odd";
+        }
+    }
+}
