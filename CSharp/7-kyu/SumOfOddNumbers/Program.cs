289e793be8cdf635208a5aa289467a9152410bc4
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-22 22:19:16 +0100

    Add SumOfOddNumbers

---
+++ CSharp/7-kyu/SumOfOddNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(rowSumOddNumbers(42));
+        }
+        public static long rowSumOddNumbers(long n)
+        {
+            return n * n * n;
+        }
+    }
+}
