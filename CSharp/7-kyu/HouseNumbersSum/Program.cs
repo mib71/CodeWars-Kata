4c424f0501a4bbf84c9ffca2743b6a438222bda2
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-23 09:31:59 +0100

    Add HouseNumbersSum

---
+++ CSharp/7-kyu/HouseNumbersSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HouseNumbersSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HouseNumbersSum(new int[] { 5, 1, 2, 3, 0, 1, 5, 0, 2 }));
+        }
+        public static int HouseNumbersSum(int[] inputArray)
+        {
+            int rv = 0;
+            foreach (int housnumber in inputArray)
+            {
+                rv += housnumber;
+                if (housnumber == 0) break;
+            }
+            return rv;
+        }
+    }
+}
