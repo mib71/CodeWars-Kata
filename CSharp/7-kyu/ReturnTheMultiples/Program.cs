f574222f3ee82c19b66706fb0713942a59e3ec06
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-01-23 18:36:30 +0100

    ReturnTheMultiples

---
+++ CSharp/7-kyu/ReturnTheMultiples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReturnTheMultiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiples(3, 5);
+        }
+        public static double[] Multiples(int m, double n)
+        {
+            double[] rv = new double[m];
+            for (int i = 0; i < m; i++)
+            {
+                rv[i] = (i + 1) * n;
+            }
+            return rv;
+        }
+    }
+}
---
+++ CSharp/7-kyu/ReturnTheMultiples/README.md
@@ -0,0 +1,18 @@
+## 7 kyu Return the first M multiples of N
+
+### Instructions
+
+Implement a function, multiples(m, n), which returns an array of the  
+first m multiples of the real number n. Assume that m is a positive integer.
+
+### Example
+```
+multiples(3, 5.0)
+```
+### should return
+```
+[5.0, 10.0, 15.0]
+```
+
+
+[Link to the Kata: Return the first M multiples of N](https://www.codewars.com/kata/593c9175933500f33400003e/train/csharp)
