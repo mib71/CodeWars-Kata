c49a7245c1a5b5b8a3f1b4e5ef1356b9d34a28fe
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-03-31 17:21:56 +0200

    Add 7 kyu Isogram

---
+++ CSharp/7-kyu/Isogram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Kata
+{
+  public static bool IsIsogram(string str) 
+  {
+    string newStr = str.ToLower();
+            if (newStr == null) return false;
+            for (int i = 0; i < (newStr.Length - 1); i++)
+            {
+                string tempChar = newStr[i].ToString();
+                for (int j = i + 1; j < (newStr.Length); j++)
+                {
+                    if (tempChar == newStr[j].ToString()) return false;
+                }
+            }
+            return true;
+  }
+}
