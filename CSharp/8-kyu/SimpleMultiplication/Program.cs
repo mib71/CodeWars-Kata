63cafe1005f5525b2b70ab00f72bbef930021334
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-13 10:32:06 +0200

    Add 8 kyu Simple multiplication

---
+++ CSharp/8-kyu/SimpleMultiplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(3));
+        }
+        public static int Multiply(int x)
+        {
+            return x % 2 == 0 ? x = x * 8 : x = x * 9;
+        }
+    }
+}
---
+++ CSharp/8-kyu/SimpleMultiplication/README.md
@@ -0,0 +1,6 @@
+## 8 kyu Simple multiplication
+
+### Description:  
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+[Link to the Kata: Simple multiplication](https://www.codewars.com/kata/583710ccaa6717322c000105/csharp)
