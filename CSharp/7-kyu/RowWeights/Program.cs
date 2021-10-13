1e09db66c1b29171b7b3750a9f7de160d578270d
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-13 16:20:32 +0200

    Add 7 kyu Row Weights

---
+++ CSharp/7-kyu/RowWeights/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace RowWeights
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var row = RowWeights(new int[] { 100, 51, 50, 100 });
+            
+            foreach (int team in row)
+            {
+                Console.WriteLine(team);
+            }
+        }
+
+        private static int[] RowWeights(int[] a)
+        {            
+            var team1 = a.Where((x, p) => p % 2 == 0).Sum();
+            var team2 = a.Where((x, p) => p % 2 == 1).Sum();
+            
+            return new int[] {team1, team2};
+        }
+    }
+}
