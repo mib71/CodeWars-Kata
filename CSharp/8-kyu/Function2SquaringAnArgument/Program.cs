301ead0a907b789e41ac6c236b03690216fe98b0
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-14 11:02:24 +0200

    Add 8 kyu Function 2 - squaring an argument

---
+++ CSharp/8-kyu/Function2SquaringAnArgument/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Function2SquaringAnArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(7));
+        }
+
+        public static double Square(double n)
+        {
+            return n * n;            
+        }
+    }
+}
