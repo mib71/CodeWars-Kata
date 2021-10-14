b6ea8d7485618c0987805763a5d7e56ed430f560
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-14 11:27:31 +0200

    Add 7 kyu Square Every Digit

---
+++ CSharp/7-kyu/SquareEveryDigit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SquareEveryDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SquareDigits(9119));
+        }
+
+        public static int SquareDigits(int n)
+        {
+            string result = "";
+            
+            foreach (var c in n.ToString())
+            {
+                int sqareIt = int.Parse(c.ToString());
+                result += (sqareIt * sqareIt);
+            }
+            
+            return int.Parse(result);
+        }
+    }
+}
---
+++ CSharp/7-kyu/SquareEveryDigit/README.md
@@ -0,0 +1,12 @@
+## 7 kyu Square Every Digit
+
+### Scenario:  
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+### Examples:
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+### Note:
+The function accepts an integer and returns an integer
+
+[Link to the Kata: Square Every Digit](https://www.codewars.com/kata/546e2562b03326a88e000020/csharp)
