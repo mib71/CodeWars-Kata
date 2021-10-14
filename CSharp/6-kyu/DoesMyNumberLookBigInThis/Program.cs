2755999e9823b1e014e27f2b924c998dbce6fb68
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-14 13:09:18 +0200

    Add 6 kyu Does my number look big in this

---
+++ CSharp/6-kyu/DoesMyNumberLookBigInThis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DoesMyNumberLookBigInThis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Narcissistic(371));
+        }
+
+        public static bool Narcissistic(int value)
+        {
+            double sum = 0;
+            int powerOf = value.ToString().Length;
+            
+            foreach (var c in value.ToString())
+            {                
+                int chr = int.Parse(c.ToString());                
+                sum += Math.Pow(chr, powerOf);                
+            }
+
+            return (sum == value) ? true : false;
+        }
+    }
+}
