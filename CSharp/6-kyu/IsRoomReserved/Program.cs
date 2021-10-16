c016f50e6b0fc37f7db60a5e894b08d1904d4952
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-16 14:16:59 +0200

    Add Is Room Reserved

---
+++ CSharp/6-kyu/IsRoomReserved/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsRoomReserved
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Interval> intervalList1 = new List<Interval>{
+                new Interval
+                {
+                    StartDate = DateTime.Parse("3/2/2019 3:04:32 PM"),
+                    EndDate = DateTime.Parse("3/5/2019 5:06:32 PM")
+                },
+                new Interval
+                {
+                    StartDate = DateTime.Parse("4/6/2019 1:04:32 PM"),
+                    EndDate = DateTime.Parse("4/6/2019 1:06:32 PM")
+                },
+            };
+
+            List<Interval> intervalList2 = new List<Interval>{
+                new Interval
+                {
+                    StartDate = DateTime.Parse("4/6/2019 1:04:32 PM"),
+                    EndDate = DateTime.Parse("4/6/2019 1:06:32 PM")
+                },
+                new Interval
+                {
+                    StartDate = DateTime.Parse("4/6/2019 1:04:32 PM"),
+                    EndDate = DateTime.Parse("4/6/2019 1:06:32 PM")
+                },
+                new Interval
+                {
+                    StartDate = DateTime.Parse("4/6/2019 1:00:32 PM"),
+                    EndDate = DateTime.Parse("4/6/2019 1:05:31 PM")
+                },
+                new Interval
+                {
+                    StartDate = DateTime.Parse("4/6/2019 1:02:33 PM"),
+                    EndDate = DateTime.Parse("4/6/2019 1:03:30 PM")
+                }
+            };
+
+            Interval testBasic = new Interval
+            {
+                StartDate = DateTime.Parse("4/6/2019 1:02:32 PM"),
+                EndDate = DateTime.Parse("4/6/2019 1:03:32 PM")
+            };
+
+            Console.WriteLine(IsRoomReserved(intervalList2, testBasic));
+        }
+
+        private static bool IsRoomReserved(List<Interval> intervals, Interval checkInterval)
+        {
+            foreach (var interval in intervals)
+            {                
+                if (((checkInterval.StartDate >= interval.StartDate && checkInterval.StartDate <= interval.EndDate)
+                    || (checkInterval.EndDate >= interval.StartDate && checkInterval.StartDate <= interval.EndDate)))
+                {
+                    return true;
+                }                
+            }
+
+            return false;
+        }
+
+        public class Interval
+        {
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+        }        
+    }
+}
---
+++ CSharp/6-kyu/IsRoomReserved/README.md
@@ -0,0 +1,20 @@
+## 6 kyu Is Room Reserved?
+
+### Details:  
+You need help booking department.
+
+You have the interval of time ( class with 2 fields: StartDate and EndDate of type DateTime) and list of intervals when room is occupied.
+
+You must check if the room is not occupied in the time frame.(boundaries are inclusive)
+
+### Examples:
+```
+Interval inter = new Interval
+{
+   StartDate = DateTime.Parse("4/6/2019 1:02:32 PM"),
+   EndDate = DateTime.Parse("4/7/2019 1:03:32 PM")
+};
+```
+
+
+[Link to the Kata: Is Room Reserved?](https://www.codewars.com/kata/5ca8cb15d7f22c002a805b29/csharp)
