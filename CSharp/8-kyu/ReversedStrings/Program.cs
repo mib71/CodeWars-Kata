e094171d6b526815858971e1bbca1a58138de4ee
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-18 11:03:48 +0200

    Add Reversed Strings

---
+++ CSharp/8-kyu/ReversedStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ReversedStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution("hello world"));
+        }
+
+        private static string Solution(string str)
+        {            
+            return string.Concat(str.Reverse());
+
+            /*
+            //First try
+
+            string reversRes = "";
+            char[] revArray = str.Reverse().ToArray();
+
+            foreach (var c in revArray)
+            {
+                reversRes += c.ToString();
+            }            
+
+            return reversRes;
+            */
+        }
+    }
+}
---
+++ CSharp/8-kyu/ReversedStrings/README.md
@@ -0,0 +1,12 @@
+## 8 kyu Reversed Strings
+
+### Details:  
+Complete the solution so that it reverses the string passed into it.
+
+### Examples:
+```
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+```
+
+[Link to the Kata: Reversed Strings](https://www.codewars.com/kata/5168bb5dfe9a00b126000018/csharp)
