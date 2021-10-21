9baaf9aaedeb21a86b40296e0c184cfe2b6e35b5
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-21 14:41:50 +0200

    Add 8 kyu Correct the mistakes of the character recognition software

---
+++ CSharp/8-kyu/CorrectTheMistakesOfTheCharacterRecognition/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CorrectTheMistakesOfTheCharacterRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Correct("51NGAP0RE"));
+        }
+
+        private static string Correct(string text)
+        {
+            return text.Replace("5", "S").Replace("0", "O").Replace("1", "I");
+        }
+    }
+}
