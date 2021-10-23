539419f4b0ac43a94deda80a5b8d608e91e6294e
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-23 14:21:19 +0200

    Add 7 kyu Youre a square

---
+++ CSharp/7-kyu/YoureASquare/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YoureASquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsSquare(121));
+        }
+
+        public static bool IsSquare(int n)
+        {
+            return Math.Sqrt(n) % 1 == 0;
+        }
+    }
+}
