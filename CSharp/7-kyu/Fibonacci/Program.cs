8671b4f83a3a418f407909a989f7c5866e4ad294
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-23 20:13:05 +0200

    Add 7 kyu Fibonacci

---
+++ CSharp/7-kyu/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(9));
+        }
+
+        private static int Fib(int n)
+        {
+            return n < 2 ? n : Fib(n - 1) + Fib(n - 2);
+        }
+        
+        //https://en.wikipedia.org/wiki/Fibonacci_number
+
+        //First try
+
+        //int fibA = 0;
+        //int fibB = 1;
+        //int res = 0;
+        //for (int i = 2; i <= n; i++)
+        //{
+        //    res = fibA + fibB;
+        //    fibA = fibB;
+        //    fibB = res;
+        //}
+
+        //return res;
+
+    }
+}
---
+++ CSharp/7-kyu/Fibonacci/README.md
@@ -0,0 +1,6 @@
+## 7 kyu Fibonacci
+
+### Details:  
+Create function fib that returns n'th element of Fibonacci sequence (classic programming task).
+
+[Link to the Kata: Fibonacci](https://www.codewars.com/kata/57a1d5ef7cb1f3db590002af/csharp)
