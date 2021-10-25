1d339e00312754520caf6ed6df69376eeed772b4
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-25 08:36:13 +0200

    Add 7 kyu The Pony Express

---
+++ CSharp/7-kyu/ThePonyExpress/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThePonyExpress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Riders(new int[] { 6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49 }));
+        }
+
+        private static int Riders(int[] stations)
+        {
+            int riders = 1;
+            int temp = 0;
+            for (int i = 0; i < stations.Length; i++)
+            {
+                temp += stations[i];
+                if (temp >= 100)
+                {
+                    temp = 0;
+                    riders++;
+                    i--;
+                }
+            }
+            
+            return riders;
+        }
+    }
+}
