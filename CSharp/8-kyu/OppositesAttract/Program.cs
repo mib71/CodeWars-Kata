ea4855e3bc4e9d8ba15ad10400b61262ffb22476
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-28 11:02:34 +0200

    Add 8 kyu Opposites Attract

---
+++ CSharp/8-kyu/OppositesAttract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OppositesAttract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lovefunc(1, 2));
+        }
+
+        private static bool lovefunc(int flower1, int flower2)
+        {
+            return ((flower1 + flower2) % 2 != 0) ? true : false;
+        }
+    }
+}
