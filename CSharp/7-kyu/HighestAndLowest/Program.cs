b7d3999203323c5e63817cf5db0b70ace8178dc0
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-10-28 12:56:56 +0200

    Add 7 kyu Highest and Lowest

---
+++ CSharp/7-kyu/HighestAndLowest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace HighestAndLowest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        }
+
+        private static string HighAndLow(string numbers)
+        {
+            string[] numberArray = numbers.Split(' ').ToArray();
+            int min = numberArray.Select(int.Parse).Min();
+            int max = numberArray.Select(int.Parse).Max();
+
+            return $"{max} {min}";
+        }
+    }
+}
