5e018048b09211561341a38424806829b4f02a82
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-04 10:07:32 +0100

    Add 7 kyu Number of People in the Bus

---
+++ CSharp/7-kyu/NumberOfPeopleInTheBus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfPeopleInTheBus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Number(new List<int[]>() { new[] { 10, 0 }, new[] { 3, 5 }, new[] { 5, 8 } }));
+        }
+
+        #region Kata
+        private static int Number(List<int[]> peopleListInOut)
+        {
+            int passengers = 0;
+            foreach (int[] stop in peopleListInOut)
+            {
+                passengers += stop[0];
+                passengers -= stop[1];                
+            }
+            return passengers;
+        }
+        #endregion
+    }
+}
