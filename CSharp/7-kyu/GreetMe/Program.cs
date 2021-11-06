1cf6b8054a096c8577c56a6266c5b3766b71af19
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-06 10:06:31 +0100

    Add 7 kyu Greet Me

---
+++ CSharp/7-kyu/GreetMe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreetMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greet("mib"));
+        }
+
+        #region Kata
+        private static string Greet(string name)
+        {
+            return $"Hello {name.ToUpper()[0] + name.ToLower()[1..]}!";
+        }
+        #endregion
+    }
+}
---
+++ CSharp/7-kyu/GreetMe/README.md
@@ -0,0 +1,12 @@
+## 7 kyu Greet Me
+
+### Details:  
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+### Example:
+```
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+```
+
+[Link to the Kata: Greet Me](https://www.codewars.com/kata/535474308bb336c9980006f2/csharp)
