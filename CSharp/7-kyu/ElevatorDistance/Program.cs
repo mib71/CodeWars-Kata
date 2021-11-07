d617db3e97ea9c3a0e460d3c00d37aab5ef33b83
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-07 09:45:45 +0100

    Add 7 kyu Elevator Distance

---
+++ CSharp/7-kyu/ElevatorDistance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ElevatorDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ElevatorDistance(new int[] { 5, 2, 8 }));
+        }
+
+        #region Kata
+        private static int ElevatorDistance(int[] array)
+        {
+            int res = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (i > 0)
+                {
+                    if (array[i] < array[i - 1])
+                        res += (array[i] - array[i - 1]) * -1;
+                    else
+                        res += (array[i] - array[i - 1]);
+                }
+            }
+
+            return res;
+        }
+        #endregion
+    }
+}
