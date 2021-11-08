dac9816d309d72bd63e6202db828161e7408b237
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-08 13:25:11 +0100

    Add 7 kyu Simple equation reversal

---
+++ CSharp/7-kyu/SimpleEquationReversal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SimpleEquationReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solve("y/b*100"));
+        }
+
+        private static string Solve(string eq)
+        {
+            return string.Join("", Regex.Split(eq, @"(\*)|(\+)|(-)|(/)").Reverse());            
+        }
+    }
+}
---
+++ CSharp/7-kyu/SimpleEquationReversal/README.md
@@ -0,0 +1,10 @@
+## 7 kyu Simple equation reversal
+
+### Details:
+Given a mathematical equation that has *,+,-,/, reverse it as follows:  
+ ```
+solve("100*b/y") = "y/b*100"
+solve("a+b-c/d*30") = "30*d/c-b+a"
+ ``` 
+
+[Link to the Kata: Simple equation reversal](https://www.codewars.com/kata/5aa3af22ba1bb5209f000037/csharp)
