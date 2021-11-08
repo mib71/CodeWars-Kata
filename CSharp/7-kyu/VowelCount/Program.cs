f2c30b11b6e6391fb54647f2c2301c1ad42b9348
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-08 08:58:50 +0100

    Add 7 kyu Vowel Count

---
+++ CSharp/7-kyu/VowelCount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace VowelCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetVowelCount("abracadabra"));
+        }
+
+        private static int GetVowelCount(string str)
+        {            
+            string vowels = "aeiou" ;
+            return str.Count(c => vowels.Contains(c));            
+        }
+    }
+}
---
+++ CSharp/7-kyu/VowelCount/README.md
@@ -0,0 +1,11 @@
+## 7 kyu Vowel Count
+
+### Details:  
+Return the number (count) of vowels in the given string.
+
+We will consider ```a, e, i, o, u``` as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+
+[Link to the Kata: Vowel Count](https://www.codewars.com/kata/54ff3102c1bad923760001f3/csharp)
