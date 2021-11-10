821e12f6a740b079b32274f13a485d3646c7d3f5
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-11 00:46:02 +0100

    Add 8 kyu Squash the bugs

---
+++ CSharp/8-kyu/SquashTheBugs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SquashTheBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindLongest("Lets all go on holiday"));
+        }
+        
+        private static int FindLongest(string str)
+        {
+            string[] spl = str.Split(' ');
+            int longest = 0;
+
+            for (int i = 0; i < spl.Length; i++)
+            {
+                if (spl[i].Length > longest)
+                {
+                    longest = spl[i].Length;
+                }
+            }
+            return longest;
+        }
+
+        /* Refactored
+        private static int FindLongest(string str)
+        {
+            return str.Split().Max(c => c.Length);
+        }
+         */
+    }
+}
