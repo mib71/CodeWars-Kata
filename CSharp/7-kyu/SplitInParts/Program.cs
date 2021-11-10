ee0453df23510d4faf0876a435cbbd030da6fc6d
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-11 00:08:58 +0100

    Add 7 kyu Split In Parts

---
+++ CSharp/7-kyu/SplitInParts/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SplitInParts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SplitInParts("supercalifragilisticexpialidocious", 3));
+        }
+
+        private static string SplitInParts(string s, int partLength)
+        {
+            for (int i = partLength; i < s.Length; i += partLength + 1)
+                s = s.Insert(i, " ");
+
+            return s;
+        }
+    }
+}
