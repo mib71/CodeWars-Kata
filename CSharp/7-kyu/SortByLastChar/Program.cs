865083da1f0b1856a6feffdd61a52fcb60478adf
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-10 09:44:53 +0100

    Add 7 kyu Sort by Last Char

---
+++ CSharp/7-kyu/SortByLastChar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SortByLastChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Last("abc cba cab"));
+        }
+
+        private static string[] Last(string x)
+        {
+            return x.Split(' ').OrderBy(x => x.Last()).ToArray();            
+        }
+    }
+}
