892d9afae1308209a774c5f2896954dcb2d09764
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-15 08:51:09 +0100

    Add 6 kyu Handshake problem

---
+++ CSharp/6-kyu/HandshakeProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HandshakeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetParticipants(100));
+        }
+
+        private static int GetParticipants(int handshakes)
+        {
+            int people = 1;
+            int handshake = 0;
+
+            while (handshakes > handshake)
+                handshake += people++;
+
+            return people;
+            /*
+             * handshakes = n(n-1)/2
+             */
+        }
+    }
+}
