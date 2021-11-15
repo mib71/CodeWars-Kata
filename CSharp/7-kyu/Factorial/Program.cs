135202df27a5f0ab5aebd85ce21d1a5bcbcc365f
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-11-15 05:48:42 +0100

    Add 7 kyu Factorial

---
+++ CSharp/7-kyu/Factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(factorial(4));
+        }
+
+        private static int factorial(int n)
+        {
+            return (n == 0) ? 1 : n * factorial(n - 1); 
+        }
+    }
+}
---
+++ CSharp/7-kyu/Factorial/README.md
@@ -0,0 +1,9 @@
+## 7 kyu Factorial
+
+### Details:
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+ 
+
+[Link to the Kata: Factorial](https://www.codewars.com/kata/57a049e253ba33ac5e000212/csharp)
