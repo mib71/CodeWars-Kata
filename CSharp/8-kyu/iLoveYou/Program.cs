d757868be4059d8b4510b87b070b8e8c04560156
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2021-12-07 21:08:18 +0100

    Add 8 kyu I love you, a little , a lot, passionately ... not at all

---
+++ CSharp/8-kyu/iLoveYou/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(HowMuchILoveYou(2));
+
+static string HowMuchILoveYou(int v)
+{
+    return new string[] { "I love you", "a little", "a lot", "passionately", 
+        "madly", "not at all" }[(v - 1) % 6];
+
+    /* First try
+    string[] phrases = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
+
+    int loops = 0;
+    if (v > phrases.Length)
+    {
+        loops = (v / phrases.Length);
+        v = (phrases.Length * loops - v) * -1;
+        if (v == 0) v = 6;
+    }
+
+    return phrases[v - 1];
+    */
+
+}
