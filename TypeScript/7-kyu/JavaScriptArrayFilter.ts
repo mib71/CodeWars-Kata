4379b929d7acbf9bc07d2be81cb5080c0d16fac0
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2022-01-25 15:02:24 +0100

    Add Refactored JavaScript Array Filter solution

--- TypeScript/7-kyu/JavaScriptArrayFilter.ts
+++ TypeScript/7-kyu/JavaScriptArrayFilter.ts
@@ -9,6 +9,13 @@ getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 https://www.codewars.com/kata/514a6336889283a3d2000001/typescript
 */
 
+// Refactored
+export const getEvenNumbers = (numbersArray: number[]): number[] => {
+  return numbersArray.filter((n) => n % 2 == 0)
+}
+
+// First try
+/*
 export const getEvenNumbers = (numbersArray: number[]): number[] => {
   let res = []
   for (let i = 0; i < numbersArray.length; i++) {
@@ -18,3 +25,4 @@ export const getEvenNumbers = (numbersArray: number[]): number[] => {
   }
   return res
 }
+*/
