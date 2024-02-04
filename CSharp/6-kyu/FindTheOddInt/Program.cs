ede7d1afae4bf1825952c7a211679d1188f287a0
mib71 <mikael.barnholdt@hotmail.com>
2024-02-04 10:54:54 +0100

    Add Kata: Find the odd int

---
+++ CSharp/6-kyu/FindTheOddInt/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FindTheOddInt
+{
+    /// <summary>
+    /// Given an array of integers, find the one that appears an odd number of times.
+    /// There will always be only one integer that appears an odd number of times.
+    /// 
+    /// [7] should return 7, because it occurs 1 time (which is odd).
+    /// [0] should return 0, because it occurs 1 time(which is odd).
+    /// [1,1,2] should return 2, because it occurs 1 time(which is odd).
+    /// [0,1,0,1,0] should return 0, because it occurs 3 times(which is odd).
+    /// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time(which is odd).
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindIt([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+        }
+
+        public static int FindIt(int[] seq) => 
+            seq.GroupBy(n => n).Single(g => g.Count() % 2 == 1).Key;        
+    }
+}
