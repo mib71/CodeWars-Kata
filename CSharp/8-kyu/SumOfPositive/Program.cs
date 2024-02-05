333c4e5f0a6a69dfe4644b57a05fff1515e22124
mib71 <mikael.barnholdt@hotmail.com>
2024-02-05 19:18:32 +0100

    Add Kata: Sum of positive

---
+++ CSharp/8-kyu/SumOfPositive/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SumOfPositive
+{
+    /// <summary>
+    /// You get an array of numbers, return the sum of all of the positives ones.
+    /// 
+    /// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PositiveSum([]));
+        }
+
+        public static int PositiveSum(int[] arr)
+        {
+            return arr.Where(n =>  n > 0).Sum();
+        }
+    }
+}
