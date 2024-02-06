f85f22a72155f6205bdec8e2c735c272fb2a0a5d
mib71 <mikael.barnholdt@hotmail.com>
2024-02-06 17:08:49 +0100

    Add Kata: Moving Zeros To The End

---
+++ CSharp/5-kyu/MovingZerosToTheEnd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MovingZerosToTheEnd
+{
+    /// <summary>
+    /// Write an algorithm that takes an array and moves all of the zeros to the end, 
+    /// preserving the order of the other elements.
+    /// 
+    /// Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}) => 
+    ///     new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MoveZeroes([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+        }
+
+        public static int[] MoveZeroes(int[] arr) => arr.OrderBy(n => n == 0).ToArray();
+    }
+}
