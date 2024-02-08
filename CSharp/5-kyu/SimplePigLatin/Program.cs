0ec352295ec3034888ee698164b77b5908c679ce
mib71 <mikael.barnholdt@hotmail.com>
2024-02-08 19:07:20 +0100

    Add Kata: Simple Pig Latin

---
+++ CSharp/5-kyu/SimplePigLatin/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimplePigLatin
+{
+    /// <summary>
+    /// Move the first letter of each word to the end of it, then add 
+    /// "ay" to the end of the word. Leave punctuation marks untouched.
+    /// 
+    /// Kata.PigIt("Pig latin is cool!"); // igPay atinlay siay oolcay!
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PigIt("Hello world !"));
+        }
+
+        public static string PigIt(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(word =>
+            {
+                if (word.All(char.IsLetter))
+                    return $"{word.Substring(1)}{word[0]}ay";
+                else
+                    return word;
+            }));            
+        }
+    }
+}
