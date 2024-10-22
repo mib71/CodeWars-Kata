cc78033a6629e8031a51e185e776cbc701fedaae
mib71 <mikael.barnholdt@hotmail.com>
2024-10-22 20:41:18 +0200

    Add GetCharacterFromASCII_Value

---
+++ CSharp/8-kyu/GetCharacterFromASCII_Value/GetCharacterFromASCII_Value/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GetCharacterFromASCII_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetChar(55));
+        }
+
+        public static char GetChar(int n) => Convert.ToChar(n);
+    }
+}
