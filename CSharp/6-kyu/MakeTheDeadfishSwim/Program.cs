f01219f23c6091aecb10b327988d1cc8f0ada7fc
mib71 <mikael.barnholdt@hotmail.com>
2024-10-23 16:36:04 +0200

    Add Make the Deadfish Swim

---
+++ CSharp/6-kyu/MakeTheDeadfishSwim/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace MakeTheDeadfishSwim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Parse("iiisdoso");
+        }
+
+        public static int[] Parse(string data)
+        {
+            List<int> output = new();
+            int i = 0;
+
+            foreach (char c in data)
+            {
+                switch (c)
+                {
+                    case 'i':
+                        i++;
+                        break;
+                    case 'd':
+                        i--;
+                        break;
+                    case 's':
+                        i *= i;
+                        break;
+                    case 'o':
+                        output.Add(i);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
