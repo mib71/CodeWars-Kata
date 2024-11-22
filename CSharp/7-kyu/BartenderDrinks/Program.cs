79247493ff9a9411b1b51c995a8c71cdb063efdf
mib71 <mikael.barnholdt@hotmail.com>
2024-11-22 05:04:42 +0100

    Add Bartender Drinks 8 Kyu

---
+++ CSharp/7-kyu/BartenderDrinks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace BartenderDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDrinkByProfession("salted Kex"));
+        }
+
+        public static string GetDrinkByProfession(string p)
+        {
+            if (words.TryGetValue(p.ToLower(), out var output))
+            {
+                return output;
+            }
+
+            return "Beer";
+        }
+
+        private static Dictionary<string, string> words = new()
+        {
+            { "jabroni", "Patron Tequila" },
+            { "school counselor", "Anything with Alcohol" },
+            { "programmer", "Hipster Craft Beer" },
+            { "bike gang member", "Moonshine" },
+            { "politician", "Your tax dollars" },
+            { "rapper", "Cristal" }	
+        };
+    }
+}
